8068c43909c308373019b7dad28a151474c7d8c8
Maja Pokos <maja.pokos@gmail.com>
2022-04-21 19:23:42 +0200

    dodali pravila igre

--- script.js
+++ script.js
@@ -4,10 +4,39 @@ const scissorsBtn = document.getElementById("scissors");
 const userChoice = [paperBtn, rockBtn, scissorsBtn];
 const computerOptions = ["rock", "paper", "scissors"];
 
-userChoice.forEach((option) => {
-    option.addEventListener ("click", () => {
-        const choiceNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[choiceNumber];
-        
+function onButtonClick() {
+    userChoice.forEach((option) => {
+        option.addEventListener ("click", (e) => {
+            const choiceNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[choiceNumber];
+            const userSelection = e.target.getAttribute("id");
+            checkWinner(userSelection, computerChoice);
+        })
     })
-})
+}
+
+function checkWinner (userChoice, computerChoice) {
+    if (computerChoice == userChoice) {
+        console.log("tie")
+    } else if (userChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("computer wins");
+        } else {
+            console.log("user wins");
+        }
+    } else if (userChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("computer wins");
+        } else {
+            console.log ("user wins");
+        }
+    } else if (userChoice == "paper") {
+        if (computerChoice == "scissors") {
+            console.log("computer wins");
+        } else {
+            console.log("user wins");
+        }
+    }
+}
+
+onButtonClick();
